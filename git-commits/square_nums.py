95a4b62a977f098b48fa79abf4b6f2258c3e07a4
naseemamin <naseem.amin@capgemini.com>
2023-08-17 13:18:40 +0100

    Fix: bugs fixed in square_nums.py

--- git-commits/square_nums.py
+++ git-commits/square_nums.py
@@ -1,13 +1,12 @@
 
 chosen_numbers = [1,3,5,7,9,11]
-squared_numbers = ()
+squared_numbers = []
 
-def square_nums_func[]:
- for x in chosen_numbers:
-   number =  x * i
-   squared_numbers.append(number)
-   squared_numbers.sort()
-   print(squared_numbers)
+def square_nums_func():
+  for x in chosen_numbers:
+    number =  x * x
+    squared_numbers.append(number)
+  squared_numbers.sort()
+  print(squared_numbers)
 
- 
 square_nums_func()
